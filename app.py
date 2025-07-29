cc2814a97f13b2dbf9fb0b93c99dc8795fafa44f
Sumit Kumar <76592937+sumit9000@users.noreply.github.com>
2025-07-29 11:06:08 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 
 # --- Page Configuration ---
 st.set_page_config(
-    page_title="Fashion AI",
+    page_title="Sumit AI",
     page_icon="✨",
     layout="centered",
     initial_sidebar_state="collapsed"
